422fb4689f75a713e74b0e27aeeebefc04436c4d
abernier <antoine.bernier@gmail.com>
2020-07-02 14:13:09 +0200

    solution

--- js/index.js
+++ js/index.js
@@ -3,40 +3,99 @@
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+  // search for different element INSIDE product
+  const $subtot = product.querySelector('.subtotal span');
+  const $pu = product.querySelector('.price span');
+  const $qty = product.querySelector('.quantity input');
+
+  // retreive values
+  const pu = Number($pu.textContent);
+  const qty = Number($qty.value) || 0;
+  const subtot = pu*qty;
+  
+  // update the product's total
+  $subtot.textContent = subtot;
+
+  return subtot;
 }
 
+const $bigtotal = document.querySelector('#total-value span');
+
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
+  let sum = 0;
 
   // ITERATION 2
-  //... your code goes here
+
+  // find all the products
+  const $products = Array.from(document.querySelectorAll('.product'));
+  
+  $products.forEach(function ($product) {
+    const tot = updateSubtotal($product);
+
+    sum += tot; // do the sum with the total returned by updateSubtotal
+  });
 
   // ITERATION 3
-  //... your code goes here
+  $bigtotal.textContent = sum; // udpate the big total
 }
 
 // ITERATION 4
 
+const $tbody = document.querySelector('#cart tbody');
+
+const $deleteButtons = Array.from(document.querySelectorAll('.btn-remove'));
+
 function removeProduct(event) {
-  const target = event.currentTarget;
+  const target = event.currentTarget; // event.currentTarget is the button clicked !!
   console.log('The target in remove is:', target);
-  //... your code goes here
+
+  // delete the product
+  const $tr = target.parentNode.parentNode; // tr product
+  $tbody.removeChild($tr);
 }
+
+function bindDeleteButton($deleteButton) {
+  $deleteButton.onclick = function (event) {
+    removeProduct(event)
+
+    calculateAll(); // once deleted, recalc all
+  };
+}
+$deleteButtons.forEach(bindDeleteButton);
 
 // ITERATION 5
 
+const $createButton = document.getElementById('create');
+const $name = document.querySelector('.create-product input[type="text"]');
+const $pu = document.querySelector('.create-product input[type="number"]');
+
 function createProduct() {
-  //... your code goes here
+  const $tr = document.createElement('tr');
+  $tr.className = 'product';
+  $tr.innerHTML = `
+  <tr class="product">
+    <td class="name">
+      <span>${$name.value}</span>
+    </td>
+    <td class="price">$<span>${$pu.value}</span></td>
+    <td class="quantity">
+      <input type="number" value="0" min="0" placeholder="Quantity" />
+    </td>
+    <td class="subtotal">$<span>0</span></td>
+    <td class="action">
+      <button class="btn btn-remove">Remove</button>
+    </td>
+  </tr>
+  `;
+  $tbody.appendChild($tr);
+
+  // don't forget to bind the .onclick handler
+  bindDeleteButton($tr.querySelector('.btn-remove'));
 }
+
+$createButton.onclick = createProduct;
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
-
-  //... your code goes here
 });
